deb68c629ce085a71f034a46dffb72919ef2a65e
Russell2204 <ruslanabdjemilov@gmail.com>
2024-01-18 23:23:28 +0500

    frst

---
+++ lesson1-bonus-TicTacToe/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+import Board from "./Board.jsx"
+
+function App() {
+  return (
+    <Board>
+      
+    </Board>
+  )
+}
+
+export default App
