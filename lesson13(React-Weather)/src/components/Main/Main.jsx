8a09ad8aa39756aff606e007b6b72011e73053ed
Russell2204 <ruslanabdjemilov@gmail.com>
2024-02-23 15:35:08 +0500

    frts

--- lesson13(React-Weather)/src/components/Main/Main.jsx
+++ lesson13(React-Weather)/src/components/Main/Main.jsx
@@ -1,21 +1,38 @@
 import React from 'react'
 
 const Main = ({ weather }) => {
-  console.log(weather.daily)
+  const weeks = weather.daily.slice(0, 7)
+  const weekDays = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
   return (
     <main className="container main">
       <h2 className="main-title">На неделю</h2>
-      <div className=" main__content">
-        {weather.daily.map((day) => (
+      <div className="main__content">
+        {weeks.map((day, idx) => (
           <div key={day.dt} className="main__content-day">
-            <h3>Сегодня</h3>
-            <h4>28 авг</h4>
+            <h3>
+              {idx === 0
+                ? 'Сегодня'
+                : weekDays[new Date(day.dt * 1000).getDay()]}
+            </h3>
+            <h4>
+              {new Date(day.dt * 1000).toLocaleDateString('ru-Ru', {
+                month: 'short',
+                day: 'numeric',
+              })}
+            </h4>
             <img
               src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
               alt=""
             />
-            <span>+{Math.floor(day.temp.day)}°</span>
-            <h5>+{Math.floor(day.feels_like.day)}°</h5>
+            <span>
+              {Math.floor(day.temp.day) < 0
+                ? Math.floor(day.temp.day)
+                : '+' + Math.floor(day.temp.day)}
+              °
+            </span>
+            <h5>  {Math.floor(day.feels_like.day) < 0
+                ? Math.floor(day.feels_like.day)
+                : '+' + Math.floor(day.feels_like.day)}°</h5>
             <h6>{day.weather[0].description}</h6>
           </div>
         ))}
